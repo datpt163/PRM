97a998dae0f056874d0473addf67f4c0265e582c
iphongnv0910 <i_phong.nv@3si.vn>
2024-11-22 13:22:09 +0700

    [Add] Skills Report

---
+++ Se_Capstone_Backend/Capstone.Application/Module/Report/Response/SkillReport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Application.Module.Report.Response
+{
+    public class SkillReport
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int UserCount { get; set; }
+        public List<UserDto> Users { get; set; } = new();
+    }
+
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+    }
+    
+}
