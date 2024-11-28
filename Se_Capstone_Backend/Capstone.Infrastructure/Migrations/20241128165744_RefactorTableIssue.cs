031ddc553adbdd8d24edc1308d826a7fe9cc8184
Phong Nguyễn <phongnv0910@gmail.com>
2024-11-28 23:58:51 +0700

    [Add] Migration SnapShot

---
+++ Se_Capstone_Backend/Capstone.Infrastructure/Migrations/20241128165744_RefactorTableIssue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Capstone.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorTableIssue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "actualDate",
+                table: "issues",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "actualDate",
+                table: "issues");
+        }
+    }
+}
