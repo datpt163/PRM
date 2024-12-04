fffdec352724eaca5b0d1d66e9290932accb20f5
datpthe163028 <133310687+datpthe163028@users.noreply.github.com>
2024-12-04 09:47:42 +0700

    [fix] project start date and end date

--- Se_Capstone_Backend/Capstone.Api/Module/Projects/Request/UpdateProjectRequest.cs
+++ Se_Capstone_Backend/Capstone.Api/Module/Projects/Request/UpdateProjectRequest.cs
@@ -8,10 +8,8 @@ namespace Capstone.Api.Module.Projects.Request
         public string Name { get; set; } = string.Empty;
         public string Code { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        [DefaultValue("2024-10-21T09:50:31.798")]
-        public DateTime StartDate { get; set; }
-        [DefaultValue("2024-10-22T09:50:31.798")]
-        public DateTime EndDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
         public ProjectStatus Status { get; set; }
         public Guid? LeadId { get; set; }
         public float? TotalEffort { get; set; }
