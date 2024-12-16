8c0f66c49539ae4a77c1262cc688b97ff97a7c47
iphongnv0910 <i_phong.nv@3si.vn>
2024-12-16 11:42:00 +0700

    [Add] Suggest Project By AI

---
+++ Se_Capstone_Backend/Capstone.Api/Module/Projects/Request/GetSuggestRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Capstone.Api.Module.Projects.Request
+{
+    public class GetSuggestRequest
+    {
+        public string SearchTerm { get; set; } = string.Empty;
+    }
+
+}
---
+++ Se_Capstone_Backend/Capstone.Application/Module/Projects/Query/GetSuggestQuery.cs
@@ -0,0 +1,10 @@
+﻿using Capstone.Application.Module.Projects.Response;
+using MediatR;
+
+namespace Capstone.Application.Module.Projects.Query
+{
+    public class GetSuggestQuery : IRequest<SuggestionResult>
+    {
+        public string SearchTerm { get; set; } = string.Empty;
+    }
+}
